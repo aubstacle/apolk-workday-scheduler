2d94bdfd6bc49cfd0cdea9ac4d876968f48cd06d
Aubrey Polk <aubstacle3@gmail.com>
2020-09-13 01:26:09 -0400

    successfully created onclick to save the time & text input to local storage

--- script.js
+++ script.js
@@ -1,37 +1,12 @@
-// // corresponding elements for the date and time in header
-
-// $(document).ready(function () {
-//   var timeList = ["07", "08", "09", "10", "11", "12", "01", "02", "03"];
-
-//   var dayEl = $("#currentDay");
-//   var timeEl = $("#currentTime");
-
-//   //Show today's date in the header using moment
-//   var m = moment();
-//   var currentDate = m.format("dddd MMMM Do");
-//   dayEl.text("Today is " + currentDate);
-//   currentTime = m.format("LTS");
-//   timeEl.text(currentTime);
-
-//   //Show live clock using moment
-// setInterval(function () {
-//   var m = moment(); // had to redefinte moment variable inside setInterval function in order to show live time
-//   var clock = m.format("h:mm:ss A");
-//   timeEl.text(clock);
-// }, 1000);
-
-//   for (var i = 0; i < timeList.length; i++);
-//   {
-//     console.log(timeList[i]);
-//   }
-
+// include everything in a document ready function to ensure the DOM is ready for script
 $(document).ready(function () {
   var dayEl = $("#currentDay");
   var timeEl = $("#currentTime");
 
-  //Show today's date in the header using moment
+  //Show today's date and live time in the header using moment
   var currentDate = moment().format("dddd MMMM Do");
   dayEl.text("Today is " + currentDate);
+
   currentTime = moment().format("LTS");
   timeEl.text(currentTime);
 
@@ -40,7 +15,7 @@ $(document).ready(function () {
     timeEl.text(clock);
   }, 1000);
 
-  var timeList = ["23", "00", "01", "02", "03", "04", "05", "06", "07"];
+  var timeList = ["09", "10", "11", "12", "13", "14", "15", "16", "17"];
 
   for (var i = 0; i < timeList.length; i++) {
     //create new rows to hold hour, text input and save button columns
@@ -53,18 +28,32 @@ $(document).ready(function () {
       "Enter description"
     );
 
+    textInput.attr("save-time", timeList[i]);
+
     var saveButton = $("<button class='col-sm-1 saveBtn'>").text("Save!");
+
+    saveButton.attr("save-time", timeList[i]);
 
     addRow.append(hourBox, textInput, saveButton);
     $(".container").append(addRow);
 
     //use moment.js to assign past, present or future lasses to each row depending on current time
-    if (timeList[i] < moment().format("HH")) {
+    currentTime = moment().format("HH");
+    if (timeList[i] < currentTime) {
       addRow.addClass("past");
-    } else if (timeList[i] > moment().format("HH")) {
+    } else if (timeList[i] > currentTime) {
       addRow.addClass("future");
     } else {
       addRow.addClass("present");
     }
   }
+
+  // save your text input and corresponding time to local storage by clicking the save button
+  $(document).on("click", ".saveBtn", function () {
+    // console.log("you clicked save")
+    localStorage.setItem(
+      $(this).attr("save-time"),
+      $(this).siblings(".description").val()
+    );
+  });
 });
