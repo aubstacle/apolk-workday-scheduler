7d5552e70952b7bb4e9f824cff002241ced47544
Aubrey Polk <aubstacle3@gmail.com>
2020-09-13 01:52:18 -0400

    applied past present future class styling to text input column instead of whole row

--- script.js
+++ script.js
@@ -19,41 +19,37 @@ $(document).ready(function () {
 
   for (var i = 0; i < timeList.length; i++) {
     //create new rows to hold hour, text input and save button columns
-    var addRow = $("<div class='row time-block'>");
-
-    var hourBox = $("<div class='col-sm-1 hour'>").text(timeList[i] + ":00");
-
+    var addRow = $("<div class='row'>");
+    var hourBox = $("<div class='col-sm-1 hour time-block'>").text(timeList[i] + ":00");
     var textInput = $("<textarea class='col-sm-10 description'>").attr(
       "placeholder",
       "Enter description"
     );
-
-    textInput.attr("save-time", timeList[i]);
-
     var saveButton = $("<button class='col-sm-1 saveBtn'>").text("Save!");
-
+    // adding this id to the button and text input is necessary for when we want to save the corresponding time of each text input in local storage
+    textInput.attr("save-time", timeList[i]);
     saveButton.attr("save-time", timeList[i]);
 
     addRow.append(hourBox, textInput, saveButton);
     $(".container").append(addRow);
 
-    //use moment.js to assign past, present or future lasses to each row depending on current time
+    // save your text input and corresponding time to local storage by clicking the save button
+    // setItem locally stores the provided key (save-time) and value (description)
+    $(document).on("click", ".saveBtn", function (store) {
+      localStorage.setItem(
+        $(this).attr("save-time"),
+        $(this).siblings(".description").val()
+      );
+    });
+
+    //use moment.js to assign past, present or future classes for each row depending on current time (makes textarea red, grey or green)
     currentTime = moment().format("HH");
     if (timeList[i] < currentTime) {
-      addRow.addClass("past");
+      textInput.addClass("past");
     } else if (timeList[i] > currentTime) {
-      addRow.addClass("future");
+      textInput.addClass("future");
     } else {
-      addRow.addClass("present");
+      textInput.addClass("present");
     }
   }
-
-  // save your text input and corresponding time to local storage by clicking the save button
-  $(document).on("click", ".saveBtn", function () {
-    // console.log("you clicked save")
-    localStorage.setItem(
-      $(this).attr("save-time"),
-      $(this).siblings(".description").val()
-    );
-  });
 });
