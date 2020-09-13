199deb10a2928f5c5dbc62b042555a382318811e
Aubrey Polk <aubstacle3@gmail.com>
2020-09-12 23:56:54 -0400

    added placeholder for text input columns, formatting

--- script.js
+++ script.js
@@ -29,15 +29,14 @@
 // });
 
 $(document).ready(function () {
-
   var dayEl = $("#currentDay");
   var timeEl = $("#currentTime");
 
   //Show today's date in the header using moment
-    var currentDate = moment().format("dddd MMMM Do");
-    dayEl.text("Today is " + currentDate);
-    currentTime = moment().format("LTS");
-    timeEl.text(currentTime);
+  var currentDate = moment().format("dddd MMMM Do");
+  dayEl.text("Today is " + currentDate);
+  currentTime = moment().format("LTS");
+  timeEl.text(currentTime);
 
   setInterval(function () {
     var clock = moment().format("h:mm:ss A");
@@ -47,18 +46,17 @@ $(document).ready(function () {
   var timeList = ["07", "08", "09", "10", "11", "12", "01", "02", "03"];
 
   for (var i = 0; i < timeList.length; i++) {
-    var addRow = $("<div class='row'>");
+    //create new rows to hold hour, text input and save button columns
+    var addRow = $("<div class='row time-block'>");
 
     var hourBox = $("<div class='col-sm-1 hour'>").text(timeList[i] + ":00");
 
-    var textInput = $("<textarea class='col-sm-10 description time-block'>");
+    var textInput = $("<textarea class='col-sm-10 description'>").attr("placeholder", "Enter description");
 
-    textInput.addClass
-
-    var saveButton = $("<button class='col-sm-1 saveBtn'>").text("Save");
-
+    var saveButton = $("<button class='col-sm-1 saveBtn'>").text("Save!");
 
     addRow.append(hourBox, textInput, saveButton);
     $(".container").append(addRow);
+    
   }
 });
