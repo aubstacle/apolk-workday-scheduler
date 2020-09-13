dfe74af7cba2347d84179d0bed77da0b130ec728
Aubrey Polk <aubstacle3@gmail.com>
2020-09-12 23:45:20 -0400

    redid for loop and got it to populate each row from array

--- script.js
+++ script.js
@@ -1,48 +1,64 @@
-//Create an array of all the time slots in your work day
-var timeArray = [
-  "07",
-  "08",
-  "09",
-  "10",
-  "11",
-  "12",
-  "1",
-  "2",
-  "3",
-];
+// // corresponding elements for the date and time in header
 
-// corresponding elements for the date and time in header
-var dayEl = $("#currentDay");
-var timeEl = $("#currentTime");
+// $(document).ready(function () {
+//   var timeList = ["07", "08", "09", "10", "11", "12", "01", "02", "03"];
 
-//Show today's date in the header using moment
-var currentDate = m.format("dddd MMMM Do");
-dayEl.text("Today is " + currentDate);
-currentTime = m.format("LTS");
-timeEl.text(currentTime);
+//   var dayEl = $("#currentDay");
+//   var timeEl = $("#currentTime");
 
-//Show live clock using moment
-setInterval(function () {
-  var m = moment(); // had to redefinte moment variable inside setInterval function in order to show live time
-  var clock = m.format("h:mm:ss A");
-  timeEl.text(clock);
-}, 1000);
+//   //Show today's date in the header using moment
+//   var m = moment();
+//   var currentDate = m.format("dddd MMMM Do");
+//   dayEl.text("Today is " + currentDate);
+//   currentTime = m.format("LTS");
+//   timeEl.text(currentTime);
+
+//   //Show live clock using moment
+// setInterval(function () {
+//   var m = moment(); // had to redefinte moment variable inside setInterval function in order to show live time
+//   var clock = m.format("h:mm:ss A");
+//   timeEl.text(clock);
+// }, 1000);
+
+//   for (var i = 0; i < timeList.length; i++);
+//   {
+//     console.log(timeList[i]);
+//   }
+
+//   // Dynamically create the new time blocks for each hour
+// });
+
+$(document).ready(function () {
+
+  var dayEl = $("#currentDay");
+  var timeEl = $("#currentTime");
+
+  //Show today's date in the header using moment
+    var currentDate = moment().format("dddd MMMM Do");
+    dayEl.text("Today is " + currentDate);
+    currentTime = moment().format("LTS");
+    timeEl.text(currentTime);
+
+  setInterval(function () {
+    var clock = moment().format("h:mm:ss A");
+    timeEl.text(clock);
+  }, 1000);
+
+  var timeList = ["07", "08", "09", "10", "11", "12", "01", "02", "03"];
+
+  for (var i = 0; i < timeList.length; i++) {
+    var addRow = $("<div class='row'>");
+
+    var hourBox = $("<div class='col-sm-1 hour'>").text(timeList[i] + ":00");
+
+    var textInput = $("<textarea class='col-sm-10 description time-block'>");
+
+    textInput.addClass
+
+    var saveButton = $("<button class='col-sm-1 saveBtn'>").text("Save");
 
 
-// Dynamically create the new time blocks for each hour
-for (var i = 0; i < timeArray.length; i++); {
-    // declare new row with hour, text input and save button columns
-    var createRow = $("<div class='row'>");
-    var hour = $("<div class='col-sm-1'>");
-    var scheduleInput = $("<textarea class='col-sm-10'>");
-    var save = $("<button class='col-sm-1 saveBtn'>");
-    var calContainer = $(".calContainer");
-
-    scheduleInput.attr("hour", timeArray[i]);
-    scheduleInput.attr("placeholder", "Enter your plans here");
-
-    createRow.append(hour, scheduleInput, save);
-    calContainer.append(createRow);
-
-    
-}
+    addRow.append(hourBox, textInput, saveButton);
+    $(".container").append(addRow);
+  }
+});
